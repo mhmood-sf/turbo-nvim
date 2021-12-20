8b07bbbc60a714246bda444a9934030fa3e97e16
quintik <28828855+quintik@users.noreply.github.com>
2021-12-21 07:36:41 +0800

    move jet config to its own file

---
+++ lua/jet-config.lua
@@ -0,0 +1,32 @@
+--- Jet configuration.
+
+local Jet = require "jet"
+
+-- Own plugins
+Jet.pack "quintik" {
+    "git@github.com:quintik/jet-nvim",
+    "git@github.com:quintik/abby-nvim",
+
+    { uri  = "git@github.com:quintik/onedark-minimal",
+      cfg  = function() vim.cmd("colorscheme onedark-minimal") end },
+}
+
+-- Nvim stuff
+Jet.pack "nvim" {
+    { name = "treesitter",
+      uri  = "git@github.com:nvim-treesitter/nvim-treesitter",
+      cfg  = function() require "config.treesitter" end },
+
+    { name = "lspconfig",
+      uri  = "git@github.com:neovim/nvim-lspconfig",
+      opt  = true,
+      on   = { "CmdUndefined" },
+      pat  = { "LspStart" },
+      cfg  = function() require "config.lsp" end }
+}
+
+-- Misc. plugins
+Jet.pack "misc" {
+    "git@github.com:ervandew/supertab"
+}
+
