f97ec34e541b82dc040eb6ad828f755bf2e9df1a
quintik <28828855+quintik@users.noreply.github.com>
2022-04-26 02:07:01 +0800

    mostly new mappings and some other small stuff

--- init.lua
+++ init.lua
@@ -1,14 +1,10 @@
---- init.lua
-
 local fn  = vim.fn
 local cmd = vim.cmd
 local map = vim.api.nvim_set_keymap
 
---[
---- Editor options
---]
-
+--[ Editor options ]--
 vim.o.sm    = true    -- showmatch: show matching brackets.
+vim.o.sc    = true    -- showcmd: show command as it is being typed
 vim.o.ic    = true    -- ignorecase: case-insensitive matching.
 vim.o.hls   = true    -- hlsesarch: highlight search results.
 vim.o.list  = true    -- list: show trailing whitespace.
@@ -26,6 +22,7 @@ vim.o.sts   = 4       -- softtabstop: 4 spaces as one tab.
 vim.o.ts    = 4       -- tabstop: number of cols in one tab char.
 vim.o.sw    = 4       -- shiftwidth: autoindent width.
 vim.o.cc    = "80"    -- colorcolumn: highlight col to help align text.
+vim.o.tw    = 80      -- textwidth: max width, longer lines are broken up.
 vim.o.spl   = "en_gb" -- spelllang: set spelling language.
 vim.o.ff    = "unix"  -- fileformat: unix file endings.
 
@@ -40,49 +37,56 @@ fn.execute "filetype plugin indent on"
 -- allow syntax highlighting
 fn.execute "syntax on"
 
---[
---- Mappings
---]
+--[ Mappings ]--
 
 -- Use space as leader.
-vim.g.mapleader = "\\"
+vim.g.mapleader = " "
+
+local opts = { noremap = true }
 
 -- Enter to insert new line without leaving normal mode
-map("n", "<Enter>", "o<Esc>", { noremap = true })
--- Avoid pressing shift all the time for ex commands
-map("n", ";", ":", { noremap = true })
--- Use F1 to exit terminal-insert mode
-map("t", "<F1>", "<C-\\><C-n>", { noremap = true })
--- Arrow keys for window sizes
-map("n", "<Up>", "<C-W>+", { noremap = true })
-map("n", "<Down>", "<C-W>-", { noremap = true })
-map("n", "<Right>", "<C-W>>", { noremap = true })
-map("n", "<Left>", "<C-W><", { noremap = true })
+map("n", "<Enter>", "o<Esc>", opts)
 
---[
---- Autocmds
---]
+-- Avoid pressing shift all the time for ex commands
+map("n", ";", ":", opts)
+
+-- Use F1 to exit terminal-insert mode
+map("t", "<F1>", "<C-\\><C-n>", opts)
+
+-- Arrow keys for window sizes
+map("n", "<Up>",    "<C-W>+", opts)
+map("n", "<Down>",  "<C-W>-", opts)
+map("n", "<Right>", "<C-W>>", opts)
+map("n", "<Left>",  "<C-W><", opts)
+
+-- H: First non-blank char of line.
+-- L: Last character of line.
+map("n", "<C-h>", "z.",     opts)
+map("n", "<C-l>", "$zz",    opts)
+
+-- J: Scroll downwards based on 'scroll' option
+-- K: Scroll upwards based on 'scroll' option
+map("n", "<C-j>", "<C-d>M", opts)
+map("n", "<C-k>", "<C-u>M", opts)
+
+-- Space to clear search highlighting
+map("n", "<Space>", "<Cmd>nohlsearch|diffupdate<CR><C-L>", opts)
+
+
+--[ Autocmds ]--
 
 -- Highlight ejs files as HTML.
 cmd "autocmd BufNewFile,BufRead *.ejs set filetype=html"
+-- Highlight sty/cls files as TeX.
+cmd "autocmd BufNewFile,BufRead *.sty set filetype=tex"
+cmd "autocmd BufNewFile,BufRead *.cls set filetype=tex"
 
---[
---- Plugins
---]
-
+--[ Plugins ]--
 require "jet-config"
 
---[
---- Mini-Plugins
---]
-
--- Auto-pairs
+--[ Mini-Plugins ]--
 require("utils.pair").create { "()", "{}", "[]", "\"\"" }
--- Zen Mode
-require "utils.zen"
--- Statusline
 require "utils.stline"
--- Terminal
-require "utils.term"
--- Misc.
 require "utils.misc"
+require "utils.zen"
+
