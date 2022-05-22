08937de042317eb0c754b049d8aee74c1a360776
quintik <28828855+quintik@users.noreply.github.com>
2022-05-23 04:45:04 +0800

    no cursorline :D

--- init.lua
+++ init.lua
@@ -3,43 +3,44 @@ local cmd = vim.cmd
 local map = vim.api.nvim_set_keymap
 
 --[ Editor options ]--
-vim.o.sm    = true    -- showmatch: show matching brackets.
-vim.o.sc    = true    -- showcmd: show command as it is being typed
-vim.o.ic    = true    -- ignorecase: case-insensitive matching.
-vim.o.hls   = true    -- hlsesarch: highlight search results.
-vim.o.list  = true    -- list: show trailing whitespace.
-vim.o.et    = true    -- expandtab: convert tabs to whitespace.
-vim.o.ai    = true    -- autoindent: indent new lines the same as previous.
-vim.o.nu    = true    -- number: show line numbers.
-vim.o.ru    = true    -- ruler: show cursor position in default statusline
-vim.o.tgc   = true    -- termguicolors: use gui colors in terminal.
-vim.o.cul   = true    -- cursorline: highlight current line.
-vim.o.udf   = true    -- undofile: save undo tree for persistent undo's.
-vim.o.title = false   -- title: don't set terminal title.
-vim.o.wrap  = false   -- wrap: dont wrap lines.
-vim.o.ls    = 2       -- laststatus: always show statusline.
-vim.o.sts   = 4       -- softtabstop: 4 spaces as one tab.
-vim.o.ts    = 4       -- tabstop: number of cols in one tab char.
-vim.o.sw    = 4       -- shiftwidth: autoindent width.
-vim.o.cc    = "80"    -- colorcolumn: highlight col to help align text.
-vim.o.tw    = 80      -- textwidth: max width, longer lines are broken up.
-vim.o.spl   = "en_gb" -- spelllang: set spelling language.
-vim.o.ff    = "unix"  -- fileformat: unix file endings.
+vim.o.sm    = true    -- showmatch: Show matching brackets.
+vim.o.sc    = true    -- showcmd: Show command as it is being typed.
+vim.o.ic    = true    -- ignorecase: Case-insensitive matching.
+vim.o.et    = true    -- expandtab: Convert tabs to whitespace.
+vim.o.ai    = true    -- autoindent: Indent new lines the same as previous.
+vim.o.nu    = true    -- number: Show line numbers.
+vim.o.ru    = true    -- ruler: Show cursor position in default statusline.
+vim.o.hls   = true    -- hlsesarch: Highlight search results.
+vim.o.tgc   = true    -- termguicolors: Use gui colors in terminal.
+vim.o.udf   = true    -- undofile: Save undo tree for persistent undo's.
+vim.o.list  = true    -- list: Show trailing whitespace.
 
--- wildmode: bash-like tab completion.
-vim.o.wim = "longest,list"
--- complete: enable dictionary words in completion.
+vim.o.cul   = false   -- cursorline: Don't highlight current line.
+vim.o.title = false   -- title: Don't set terminal title.
+vim.o.wrap  = false   -- wrap: Dont wrap lines.
+
+vim.o.ls    = 2       -- laststatus: Always show statusline.
+vim.o.sts   = 4       -- softtabstop: 4 spaces as one tab.
+vim.o.ts    = 4       -- tabstop: Number of cols in one tab char.
+vim.o.sw    = 4       -- shiftwidth: Autoindent width.
+vim.o.tw    = 80      -- textwidth: Max width, longer lines are broken up.
+vim.o.cc    = "80"    -- colorcolumn: Highlight col to help align text.
+vim.o.ff    = "unix"  -- fileformat: Unix file endings.
+vim.o.spl   = "en_gb" -- spelllang: Set spelling language.
+
+-- complete: Enable dictionary words in completion.
 vim.opt.cpt:append { "k" }
--- undodir: directory for the undofile
+-- wildmode: Bash-like tab completion.
+vim.o.wim = "longest,list"
+-- undodir: Directory for the undofile
 vim.o.udir = fn.stdpath("data") .. "/undo/"
--- detect filetypes and load filetype plugins and indent files
+
+-- Detect filetypes and load filetype plugins and indent files
 fn.execute "filetype plugin indent on"
--- allow syntax highlighting
+-- Allow syntax highlighting
 fn.execute "syntax on"
 
 --[ Mappings ]--
-
--- Use space as leader.
 vim.g.mapleader = " "
 
 local opts = { noremap = true }
@@ -80,12 +81,9 @@ map("n", "<Esc>", "<Cmd>nohlsearch|diffupdate<CR><C-L>", opts)
 
 
 --[ Autocmds ]--
-
--- Highlight ejs files as HTML.
-cmd "autocmd BufNewFile,BufRead *.ejs set filetype=html"
--- Highlight sty/cls files as TeX.
-cmd "autocmd BufNewFile,BufRead *.sty set filetype=tex"
-cmd "autocmd BufNewFile,BufRead *.cls set filetype=tex"
+cmd "autocmd BufNewFile,BufRead *.ejs set syntax=html"
+cmd "autocmd BufNewFile,BufRead *.sty set syntax=tex"
+cmd "autocmd BufNewFile,BufRead *.cls set syntax=tex"
 
 --[ Plugins ]--
 require "jet-config"
@@ -95,4 +93,3 @@ require("utils.pair").create { "()", "{}", "[]", "\"\"" }
 require "utils.stline"
 require "utils.misc"
 require "utils.zen"
-
