4142a4d55ccf0f7aefd0a62185ce852fa687885b
quintik <28828855+quintik@users.noreply.github.com>
2022-09-21 05:33:40 +0800

    update vault functions

--- lua/utils/vault.lua
+++ lua/utils/vault.lua
@@ -1,8 +1,9 @@
 local VAULT_PATH = os.getenv("HOME") .. "/Personal/Vault"
-local TASKS_PATH = VAULT_PATH .. "/Life/Tasks"
-local INBOX_PATH = VAULT_PATH .. "/Life/Inbox"
+local INBOX_PATH = VAULT_PATH .. "/INBOX"
+local TASKS_FILE = VAULT_PATH .. "/Life/tasks.txt"
+local REFS_PATH  = VAULT_PATH .. "/Life/references"
 
-local function open_today_note()
+local function new_today_note()
     local note_name = vim.fn.strftime("%Y-%m-%d.md")
     local note_path = INBOX_PATH .. "/" .. note_name
 
@@ -33,29 +34,61 @@ local function open_today_note()
     end
 
     vim.cmd("edit " .. note_path)
-    vim.cmd "normal G"
+    vim.cmd "normal G$a"
 end
 
-local function open_pending_tasks()
-    local path = TASKS_PATH .. "/Pending.md"
+local function new_pending_task()
+    local date = vim.fn.strftime("%d-%m-%Y")
+    local time = vim.fn.strftime("%H:%M")
+
+    vim.fn.writefile({
+        "9 | " .. date .. " | " .. time .. " | ",
+    }, TASKS_FILE, "a")
+
+    vim.cmd("edit " .. TASKS_FILE)
+    vim.cmd "normal G$"
+end
+
+local function new_life_ref()
+    local note_name = vim.fn.strftime("%Y-%m-%d.md")
+    local note_path = REFS_PATH .. "/" .. note_name
+
+    local contents = vim.fn.readdir(REFS_PATH)
+    local exists
+    for _, name in ipairs(contents) do
+        if name == note_name then
+            exists = true
+        end
+    end
 
     local date = vim.fn.strftime("%d %b, %Y")
     local time = vim.fn.strftime("%H:%M")
 
-    vim.fn.writefile({
-        "",
-        "## " .. time .. " | " .. date,
-        ""
-    }, path, "a")
+    if not exists then
+        vim.fn.writefile({
+            "# " .. date,
+            "",
+            "## " .. time,
+            ""
+        }, note_path)
+    else
+        vim.fn.writefile({
+            "",
+            "## " .. time,
+            ""
+        }, note_path, "a")
+    end
 
-    vim.cmd("edit " .. path)
+    vim.cmd("edit " .. note_path)
     vim.cmd "normal G"
 end
 
-vim.cmd "command! Note lua require\"utils.vault\".open_today_note()"
-vim.cmd "command! Task lua require\"utils.vault\".open_pending_tasks()"
+vim.cmd "command! Note lua require\"utils.vault\".new_today_note()"
+vim.cmd "command! Task lua require\"utils.vault\".new_pending_task()"
+vim.cmd "command! Ref  lua require\"utils.vault\".new_life_ref()"
 
 return {
-    open_today_note = open_today_note,
-    open_pending_tasks = open_pending_tasks
+    new_life_ref = new_life_ref,
+    new_today_note = new_today_note,
+    new_pending_task = new_pending_task
 }
